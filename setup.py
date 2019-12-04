97274defdcc98d0364ffa8da59e0466f89cbac75
Joe Garcia <joe@joe-garcia.com>
2019-12-04 15:34:05 -0500

    initial commit

---
+++ setup.py
@@ -0,0 +1,61 @@
+import os.path
+
+import setuptools
+
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+PACKAGE_NAME = "pypsm"
+CURRENT_DIR = os.path.abspath(os.path.dirname(__file__))
+VERSION_FILE = os.path.join(CURRENT_DIR, PACKAGE_NAME, "version.py")
+
+VERSION_DATA = {}
+with open(VERSION_FILE, "r") as version_fp:
+    exec(version_fp.read(), VERSION_DATA)
+
+setuptools.setup(
+    name="pypsm",
+    version=VERSION_DATA['__version__'],
+    author="Joe Garcia",
+    author_email="joe.garcia@cyberark.com",
+    description="CyberArk Privileged Session Manager RDPFile Generator",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/infamousjoeg/pypsm",
+    license="MIT",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3 :: Only",
+        "Development Status :: 4 - Beta",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+        "Natural Language :: English",
+        "Topic :: Office/Business",
+        "Topic :: Security",
+        "Topic :: Security :: Cryptography",
+        "Topic :: Software Development",
+        "Topic :: Software Development :: Libraries",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+        "Topic :: System :: Systems Administration",
+        "Topic :: Utilities",
+    ],
+    keywords=[
+        "cyberark",
+        "security",
+        "vault",
+        "psm",
+        "rdp",
+        "privileged access",
+        "identity",
+        "pam",
+        "pim",
+        "pas"
+        ],
+    project_urls={
+        'Bug Reports': 'https://github.com/infamousjoeg/pypsm/issues/new?assignees=&labels=&template=bug_report.md&title=',
+        'Feature Requests': 'https://github.com/infamousjoeg/pypsm/issues/new?assignees=&labels=&template=feature_request.md&title=',
+        'Source': 'https://github.com/infamousjoeg/pypsm'
+    }
+)
